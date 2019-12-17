ac0b24bf8645cc1b9cbdd4b870b53b7383fcf8fb
AymericM <aymeric.moehn@gmail.com>
2019-12-17 11:37:23 +0100

    Add data structure and items mutators

--- src/stores/MainStore.js
+++ src/stores/MainStore.js
@@ -7,7 +7,52 @@ class MainProvider extends Component {
     constructor() {
         super()
         this.state = {
-            activeScreen: 'home'
+            activeScreen: 'home',
+            planning: [
+                {
+                    name: 'Jean-Clodo',
+                    hours: '12h',
+                    descr: 'Un café svp chef'
+                }
+            ],
+            data: {
+                balance: 132.0,
+                items: [
+                    {
+                        id: 1,
+                        name: 'Café',
+                        amount: 1.0
+                    },
+                    {
+                        id: 2,
+                        name: 'Cookie',
+                        amount: 2.5
+                    },
+                    {
+                        id: 3,
+                        name: 'Chocolat chaud',
+                        amount: 2.8
+                    }
+                ],
+                profile: {
+                    name: `Café de l'arche`,
+                    adress: '',
+                    mail: '',
+                    type: '',
+                    hours: [
+                        {
+                            day: 'Lundi',
+                            closed: false,
+                            hours: [9, 23]
+                        },
+                        {
+                            day: 'Mardi',
+                            closed: true,
+                            hours: []
+                        }
+                    ]
+                }
+            }
         }
 
         this.actions = {
@@ -17,6 +62,30 @@ class MainProvider extends Component {
 
     navigate(name) {
         this.setState({ activeScreen: name })
+    }
+
+    addItem(data) {
+        const newItems = this.state.items.push(data)
+
+        this.setState({
+            data: {
+                ...this.state.data,
+                items: newItems
+            }
+        })
+    }
+
+    removeItem(id) {
+        const newItems = this.state.items.filter(a => {
+            a.id != id
+        })
+
+        this.setState({
+            data: {
+                ...this.state.data,
+                items: newItems
+            }
+        })
     }
 
     render () {
