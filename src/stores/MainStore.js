ab29d1ce390f2c7e8dfd1e6fac587d10e2d68787
AymericM <aymeric.moehn@gmail.com>
2019-12-18 11:18:39 +0100

    Change default homescreen

--- src/stores/MainStore.js
+++ src/stores/MainStore.js
@@ -7,7 +7,7 @@ class MainProvider extends Component {
     constructor() {
         super()
         this.state = {
-            activeScreen: 'donations',
+            activeScreen: 'balance',
             planning: [
                 {
                     name: 'Jean-Clodo',
